d7f5c0fc8c179ac50e1dbd896bafde11cc9cab39
Alex Benton <alex@bentonian.com>
2014-06-11 22:46:27 +0100

    Redux isn't so redux now.  But this is work in progress.

--- src/main/java/org/saintandreas/vr/demo/RiftDemoRedux.java
+++ src/main/java/org/saintandreas/vr/demo/RiftDemoRedux.java
@@ -1,50 +1,89 @@
 package org.saintandreas.vr.demo;
 
-import static java.lang.Math.*;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.saintandreas.gl.MatrixStack.MODELVIEW;
 
-import org.saintandreas.gl.MatrixStack;
 import org.saintandreas.gl.OpenGL;
 import org.saintandreas.math.Vector3f;
 import org.saintandreas.vr.RiftApp;
 
+import com.oculusvr.capi.OvrLibrary;
+
 public class RiftDemoRedux extends RiftApp {
 
+  private final float ipd;
+  private final float eyeHeight;
+  
+  public RiftDemoRedux() {
+    ipd = hmd.getFloat(OvrLibrary.OVR_KEY_IPD, 
+        OvrLibrary.OVR_DEFAULT_IPD);
+    eyeHeight = hmd.getFloat(
+        OvrLibrary.OVR_KEY_EYE_HEIGHT, 
+        OvrLibrary.OVR_DEFAULT_EYE_HEIGHT);
+  }
+  
   @Override
   protected void initGl() {
     super.initGl();
     glEnable(GL_DEPTH_TEST);
-    glClearColor(0.2f, 0.2f, 0.2f, 1);
+    glClearColor(0.529f, 0.807f, 0.921f, 1); // sky blue
 
-    MatrixStack.MODELVIEW.lookat(
-        Vector3f.ZERO,            // eye position
-        Vector3f.UNIT_Z.mult(-5), // point to look at
+    MODELVIEW.lookat(
+        new Vector3f(0, eyeHeight, 0.5f), // eye position
+        new Vector3f(0, eyeHeight, 0), // point to look at
         Vector3f.UNIT_Y);         // up direction
-
   }
 
   @Override
   public void renderScene() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    Vector3f scale = new Vector3f(
+        0.975f, 0.975f, 0.975f);
     
-    for (int x = -5; x <= 5; x++) {
-      for (int y = -5; y <= 5; y++) {
-        for (int z = -5; z <= 5; z++) {
-          if (abs(x * y) == 25
-              || abs(y * z) == 25
-              || abs(x * z) == 25
-              || y == -5) {
-            cube(new Vector3f(x, y, z));
+    // Draw arches made of cubes.
+    for (int x = -10; x <= 10; x++) {
+      for (int y = 0; y <= 10; y++) {
+        for (int z = -10; z <= 10; z++) {
+          Vector3f pos = new Vector3f(x, y, z);
+          float len = pos.length();
+          if (len > 11 && len < 12) {
+            MODELVIEW.push().translate(pos);
+            OpenGL.drawColorCube();
+            MODELVIEW.pop();
           }
         }
       }
     }
-  }
-  
-  private void cube(Vector3f pos) {
-    MatrixStack.MODELVIEW.push().translate(pos);
+    
+    // Draw a floor made of cubes.
+    for (int x = -10; x <= 10; x++) {
+      for (int z = -10; z <= 10; z++) {
+        Vector3f pos = new Vector3f(x, -0.5f, z);
+        MODELVIEW.push().translate(pos).scale(scale);
+        OpenGL.drawColorCube();
+        MODELVIEW.pop();
+      }
+    }
+    
+    // Draw a single cube at the center of the room,
+    // at eye height.
+    MODELVIEW.push()
+        .translate(new Vector3f(0, eyeHeight, 0))
+        .scale(ipd);
     OpenGL.drawColorCube();
-    MatrixStack.MODELVIEW.pop();
+    MODELVIEW.pop();
+
+    // Put the cube on a pedestal.  (Any point?)
+    MODELVIEW.push()
+        .translate(new Vector3f(0, eyeHeight / 2, 0))
+        .scale(new Vector3f(ipd / 2, eyeHeight, ipd / 2));
+    OpenGL.drawColorCube();
+    MODELVIEW.pop();
   }
 
   public static void main(String[] args) {
